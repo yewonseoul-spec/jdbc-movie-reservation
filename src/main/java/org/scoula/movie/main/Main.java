e62eae5d92184023c70e6f2e60ce4b7d10b1e18f
ihyoneung <lee33750519@gmail.com>
2026-06-04 23:18:01 +0900

    feat: 영화 예매 기능 구현 및 UI 개선

--- src/main/java/org/scoula/movie/main/Main.java
+++ src/main/java/org/scoula/movie/main/Main.java
@@ -1,6 +1,8 @@
 package org.scoula.movie.main;
 
-public class  Main {
+import org.scoula.movie.view.MainMenuView;
+
+public class Main {
     public static void main(String[] args) {
         MainMenuView mainMenuView = new MainMenuView();
         mainMenuView.run();
